fee0fa98f261aa20431e375b9ff64dc2f3f69371
Abde Tiamani <tiamani3939@gmail.com>
2024-09-01 16:08:42 +0100

    first commit

--- components/ui/Stars.tsx
+++ components/ui/Stars.tsx
@@ -1,7 +1,7 @@
 import { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import * as random from "maath/random";
 
 const Stars = (props) => {
   const ref = useRef();
