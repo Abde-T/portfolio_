b0b155bfe105ba706909bcdf0ef02fb7395d1d2c
Abderrahmane <tiamani3939@gmail.com>
2025-01-24 09:01:45 +0100

    optimisations---

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -55,14 +55,6 @@ export const BentoGridItem = ({
 
   const [copied, setCopied] = useState(false);
 
-  const defaultOptions = {
-    loop: copied,
-    autoplay: copied,
-    animationData: animationData|| {}, 
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
 
   const handleCopy = () => {
     const text = "tiamani3939@gmail.com";
